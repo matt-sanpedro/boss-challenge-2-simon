f13c46fe50182f952660c7ad0a0f59e016b6e9ad
matt-sanpedro <mattsanpedro76@gmail.com>
2020-10-21 08:45:57 -0700

    added random number generator and random color picker sequence

---
+++ game.js
@@ -0,0 +1,21 @@
+// array to hold the four colors
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+// function: generate a random number 0-3
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random()*4);
+  // // min
+  // console.log(Math.floor(0*4));
+  // // max
+  // console.log(Math.floor(.99999*4));
+
+  return randomNumber;
+}
+
+var randomChosenColour = buttonColours[nextSequence()];
+
+gamePattern.push(randomChosenColour);
+
+console.log(gamePattern);
