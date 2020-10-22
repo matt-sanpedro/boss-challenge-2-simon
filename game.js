c9d8c94296ed1f6643e794d00ab864782875575b
matt-sanpedro <mattsanpedro76@gmail.com>
2020-10-22 00:24:27 -0700

    added sound to buttons

--- game.js
+++ game.js
@@ -1,23 +1,25 @@
 // array to hold the four colors
-var buttonColours = ["red", "blue", "green", "yellow"];
+
 
 var gamePattern = [];
 
 // function: generate a random number 0-3
 function nextSequence() {
-  var randomNumber = Math.floor(Math.random()*4);
   // // min
   // console.log(Math.floor(0*4));
   // // max
   // console.log(Math.floor(.99999*4));
+  var randomNumber = Math.floor(Math.random() * 4);
+  var buttonColours = ["red", "blue", "green", "yellow"];
+  var randomChosenColour = buttonColours[randomNumber];
+  var fadeDuration = 350;
 
-  return randomNumber;
+  $("#" + randomChosenColour).fadeOut(fadeDuration).fadeIn(fadeDuration).fadeOut(fadeDuration).fadeIn(fadeDuration);
+
+
+  // return randomNumber;
+  var playSound = new Audio("sounds/" + randomChosenColour + ".mp3");
+  playSound.play();
 }
 
-// choose random color and add to end of gamePattern list
-var randomChosenColour = buttonColours[nextSequence()];
-gamePattern.push(randomChosenColour);
-
-// console.log(gamePattern);
-
-// jQuery select button with same id
+nextSequence();
