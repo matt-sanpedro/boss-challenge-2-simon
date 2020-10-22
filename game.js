4d024f86b117de2c2a1613eb02fb450ef66ab4c3
matt-sanpedro <mattsanpedro76@gmail.com>
2020-10-22 00:51:45 -0700

    added functionality - store the id (color) of the button that got clicked

--- game.js
+++ game.js
@@ -9,17 +9,27 @@ function nextSequence() {
   // console.log(Math.floor(0*4));
   // // max
   // console.log(Math.floor(.99999*4));
-  var randomNumber = Math.floor(Math.random() * 4);
-  var buttonColours = ["red", "blue", "green", "yellow"];
-  var randomChosenColour = buttonColours[randomNumber];
-  var fadeDuration = 350;
 
+  // generate random number and color
+  var randomNumber = Math.floor(Math.random() * 4);
+  var buttonColours = ["green", "red", "yellow", "blue"];
+  var randomChosenColour = buttonColours[randomNumber];
+
+
+  // play animation, set the fade duration
+  var fadeDuration = 100;
   $("#" + randomChosenColour).fadeOut(fadeDuration).fadeIn(fadeDuration).fadeOut(fadeDuration).fadeIn(fadeDuration);
 
-
-  // return randomNumber;
+  // play sound
   var playSound = new Audio("sounds/" + randomChosenColour + ".mp3");
   playSound.play();
+
 }
 
 nextSequence();
+
+// event listener
+$(".btn").click(function() {
+  var userChosenColour = $(this).attr("id");
+  console.log(userChosenColour);
+});
