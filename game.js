6bdeaefe9504448e2d3640c9b19be4aec25b25f1
matt-sanpedro <mattsanpedro76@gmail.com>
2020-10-26 23:21:20 -0700

    added check answer functionality, game is playable

--- game.js
+++ game.js
@@ -7,6 +7,7 @@ var level = 0;
 
 // function: generate a random number 0-3
 function nextSequence() {
+  userClickedPattern = [];
   // // min
   // console.log(Math.floor(0*4));
   // // max
@@ -16,6 +17,7 @@ function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var buttonColours = ["green", "red", "yellow", "blue"];
   var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
 
 
   // play animation, set the fade duration
@@ -27,6 +29,11 @@ function nextSequence() {
   level+=1;
   $("h1").text("Level " + level);
   // console.log(level);
+
+
+
+  // checkAnswer(userClickedPattern.length-1);
+
 }
 
 function playSound(name) {
@@ -42,19 +49,37 @@ function animatePress(currentColor) {
   }, 100);
 }
 
-// event listener: button click
-$(".btn").click(function() {
-  var userChosenColour = $(this).attr("id");
-  // console.log(userChosenColour);
-  userClickedPattern.push(userChosenColour);
-  // console.log(userClickedPattern);
-
-  playSound(userChosenColour);
-  animatePress(userChosenColour);
-});
-
 // event listener: press any key to start
 $(document).keypress(function() {
   // console.log($(this));
   $(this).on(nextSequence());
 });
+
+// event listener: button click
+$(".btn").click(function() {
+  var userChosenColour = $(this).attr("id");
+  // console.log(userChosenColour);
+  userClickedPattern.push(userChosenColour);
+  // console.log(userClickedPattern.length-1);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length-1);
+
+  if (userClickedPattern.length === gamePattern.length) {
+    setTimeout(function () {
+      nextSequence();
+    }, 1000);
+  }
+});
+
+// check answer function
+function checkAnswer(currentLevel) {
+  // console.log(currentLevel);
+  console.log(userClickedPattern, gamePattern);
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    console.log('Success');
+  } else {
+    console.log('Wrong');
+  }
+}
